dd1e6e5b7cac757b1db94db7616c53d165dfe705
Malin Øien <but005@uib.no>
2018-03-21 13:22:31 +0100

    Lab onsdag

--- js/toalett.js
+++ js/toalett.js
@@ -1,258 +1,3 @@
-var toalettliste = [{
-         "herre":"1",
-         "tid_sondag":"07.00 - 23.15",
-         "pissoir_only":"NULL",
-         "stellerom":"NULL",
-         "latitude":"60.388076",
-         "tid_hverdag":"07.00 - 23.15",
-         "plassering":"NONNESETER TERMINAL, SØR",
-         "tid_lordag":"07.00 - 23.15",
-         "rullestol":"1",
-         "adresse":"Lungegårdskaien",
-         "pris":"12",
-         "id":"1",
-         "place":"NONNESETER TERMINAL, SOUTH",
-         "dame":"1",
-         "longitude":"5.334937"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"NULL",
-         "pissoir_only":"NULL",
-         "stellerom":"NULL",
-         "latitude":"60.388871",
-         "tid_hverdag":"05.30 - 23.50",
-         "plassering":"NONNESETER TERMINAL , NORD",
-         "tid_lordag":"07.00 - 23.15",
-         "rullestol":"1",
-         "adresse":"Østre Strømkai",
-         "pris":"12",
-         "id":"2",
-         "place":"NONNESETER TERMINAL , NORTH",
-         "dame":"1",
-         "longitude":"5.334079"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"NULL",
-         "pissoir_only":"NULL",
-         "stellerom":"NULL",
-         "latitude":"60.388868",
-         "tid_hverdag":"09.00 - 17.00",
-         "plassering":"SKYSS KUNDESENTER",
-         "tid_lordag":"09.00 - 15.00",
-         "rullestol":"1",
-         "adresse":"Østre Strømkai",
-         "pris":"12",
-         "id":"3",
-         "place":"SKYSS CUSTOMER CENTRE",
-         "dame":"1",
-         "longitude":"5.334079"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"07.00 - 23.00",
-         "pissoir_only":"NULL",
-         "stellerom":"NULL",
-         "latitude":"60.39041",
-         "tid_hverdag":"07.00 - 23.00",
-         "plassering":"JERNBANESTASJONEN",
-         "tid_lordag":"07.00 - 23.00",
-         "rullestol":"NULL",
-         "adresse":"Strømgaten 4",
-         "pris":"10",
-         "id":"4",
-         "place":"RAILWAY STATION",
-         "dame":"1",
-         "longitude":"5.332995"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"08.30 - 22.00",
-         "pissoir_only":"NULL",
-         "stellerom":"1",
-         "latitude":"60.394554",
-         "tid_hverdag":"09.00 - 23.00",
-         "plassering":"MATHALLEN",
-         "tid_lordag":"08.30 - 22.00",
-         "rullestol":"1",
-         "adresse":"Strandkaien 3",
-         "pris":"10",
-         "id":"5",
-         "place":"FISH MARKET",
-         "dame":"1",
-         "longitude":"5.324099"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"08.00 - 18.00",
-         "pissoir_only":"NULL",
-         "stellerom":"",
-         "latitude":"60.395432",
-         "tid_hverdag":"08.00 - 18.00",
-         "plassering":"STRANDKAITERMINALEN",
-         "tid_lordag":"08.00 - 18.00",
-         "rullestol":"",
-         "adresse":"Strandkaien",
-         "pris":"10",
-         "id":"6",
-         "place":"STRANDKAI BOAT TERMINAL",
-         "dame":"1",
-         "longitude":"5.321034"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"NULL",
-         "pissoir_only":"NULL",
-         "stellerom":"NULL",
-         "latitude":"60.3913929",
-         "tid_hverdag":"08.00 - 15.00",
-         "plassering":"BERGEN KOMMUNE, INNBYGGERSERVICE",
-         "tid_lordag":"NULL",
-         "rullestol":"1",
-         "adresse":"Kaigaten 4",
-         "pris":"0",
-         "id":"7",
-         "place":"CITIZEN SERVICE CENTRE",
-         "dame":"1",
-         "longitude":"5.326855"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"NULL",
-         "pissoir_only":"NULL",
-         "stellerom":"1",
-         "latitude":"60.3891794",
-         "tid_hverdag":"09.00 - 21.00",
-         "plassering":"BERGEN STORSENTER",
-         "tid_lordag":"09.00 - 18.00",
-         "rullestol":"1",
-         "adresse":"Strømgaten 8",
-         "pris":"10",
-         "id":"8",
-         "place":"BERGEN STORSENTER",
-         "dame":"1",
-         "longitude":"5.3305793"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"NULL",
-         "pissoir_only":"NULL",
-         "stellerom":"1",
-         "latitude":"60.392209",
-         "tid_hverdag":"09.00 - 21.00",
-         "plassering":"SUNDT MOTEHUS",
-         "tid_lordag":"09.00 - 18.00",
-         "rullestol":"1",
-         "adresse":"Torgallmenningen 14",
-         "pris":"10",
-         "id":"9",
-         "place":"SUNDT FASHION HOUSE",
-         "dame":"1",
-         "longitude":"5.324011"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"NULL",
-         "pissoir_only":"NULL",
-         "stellerom":"1",
-         "latitude":"60.3928444",
-         "tid_hverdag":"09.00 - 20.00",
-         "plassering":"XHIBITION",
-         "tid_lordag":"09.00 - 18.00",
-         "rullestol":"1",
-         "adresse":"Småstrandgaten 3",
-         "pris":"10",
-         "id":"10",
-         "place":"XHIBITION",
-         "dame":"1",
-         "longitude":"5.3239541"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"NULL",
-         "pissoir_only":"NULL",
-         "stellerom":"1",
-         "latitude":"60.3947371",
-         "tid_hverdag":"09.00 - 21.00",
-         "plassering":"GALLERIET",
-         "tid_lordag":"09.00 - 18.00",
-         "rullestol":"1",
-         "adresse":"Torgallmenningen 8",
-         "pris":"10",
-         "id":"11",
-         "place":"GALLERIET",
-         "dame":"1",
-         "longitude":"5.3243561"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"NULL",
-         "pissoir_only":"NULL",
-         "stellerom":"1",
-         "latitude":"60.3944194",
-         "tid_hverdag":"10.00 - 20.00",
-         "plassering":"KLØVERHUSET",
-         "tid_lordag":"10.00 - 18.00",
-         "rullestol":"1",
-         "adresse":"Strandgaten 13 -15",
-         "pris":"10",
-         "id":"12",
-         "place":"KLØVERHUSET",
-         "dame":"1",
-         "longitude":"5.3205649"
-      },
-      {
-         "herre":"1",
-         "tid_sondag":"09.00 - 18.00",
-         "pissoir_only":"NULL",
-         "stellerom":"NULL",
-         "latitude":"60.3974108",
-         "tid_hverdag":"09.00 - 18.00",
-         "plassering":"BRYGGEN BESØKSSENTER",
-         "tid_lordag":"09.00 - 18.00",
-         "rullestol":"1",
-         "adresse":"Jacobsfjorden, Bryggen",
-         "pris":"10",
-         "id":"13",
-         "place":"BRYGGEN VISITOR CENTRE",
-         "dame":"1",
-         "longitude":"5.3222145"
-      },
-      {
-         "herre":"NULL",
-         "tid_sondag":"ALL",
-         "pissoir_only":"1",
-         "stellerom":"NULL",
-         "latitude":"60.397359",
-         "tid_hverdag":"ALL",
-         "plassering":"C. SUNDTSGT",
-         "tid_lordag":"ALL",
-         "rullestol":"NULL",
-         "adresse":"C. Sundts gt",
-         "pris":"NULL",
-         "id":"14",
-         "place":"C. SUNDTSGT",
-         "dame":"NULL",
-         "longitude":"5.313963"
-      },
-      {
-         "herre":"NULL",
-         "tid_sondag":"ALL",
-         "pissoir_only":"1",
-         "stellerom":"NULL",
-         "latitude":"60.397557",
-         "tid_hverdag":"ALL",
-         "plassering":"NORDNES SKOLE",
-         "tid_lordag":"ALL",
-         "rullestol":"NULL",
-         "adresse":"Nordnesparken 3",
-         "pris":"NULL",
-         "id":"15",
-         "place":"NORDNES SKOLE",
-         "dame":"NULL",
-         "longitude":"5.307858"}];
-
 //  Gjør at scriptet ikke prøver å finne elementer før siden
 //  er ferdig innlastet, og unngår null-pointer
 window.onload = function(){
@@ -282,13 +27,12 @@ window.onload = function(){
     } else {
         topNav.className = "navbar";
     }
-}
+  }
 
 // Viser og skjuler avansert søk ved klikk
  function visSøk(){
     var knapp = document.getElementById("avansertSøkKnapp");
     if(knapp){
-      console.log("Knapp fungerer!");
       knapp.addEventListener("click", function(){
         var avansert = document.getElementById("boksTilSøk");
         if (avansert.style.display === "flex") {
@@ -303,10 +47,16 @@ window.onload = function(){
     }
     //gir filtrerknappen funksjon
     var filtrerSøkKnapp = document.getElementById("filtrerSøk");
-    filtrerSøkKnapp.addEventListener("click", avansertSøk);
+    filtrerSøkKnapp.addEventListener("click", skjemaSøk);
     //gir fjern-filter-knappen funksjon
     document.getElementById("tilbakestillSøk").addEventListener("click", tilbakestillSøk);
-
+    //gir hurtigsøk en funksjon
+    document.getElementById("hurtigsøk").addEventListener("keypress", function(e){
+      var key = e.which || e.keyCode;
+      if (key == 13){
+        hurtigSøk();
+      }
+    });
 }
 //Array til å lagte markørene i
 var markerArray = [];
@@ -396,8 +146,8 @@ function createElement(element) {
     tid_søndag.classList.add("tid_søndag");
     tid_søndag.innerHTML = element.tid_sondag;
     rekke.appendChild(tid_søndag);
-
-    // oppretter span-element for dame
+  //Her har vi også muligheten til å legge inn kjønn i tabellen
+  /*  // oppretter span-element for dame
     var dame = document.createElement("td");
     dame.classList.add("dame");
     dame.innerHTML = element.dame;
@@ -408,6 +158,8 @@ function createElement(element) {
     herre.classList.add("herre");
     herre.innerHTML = element.herre;
     rekke.appendChild(herre);
+
+    */
 
     // oppretter span-element for rullestol
     var rullestol = document.createElement("td");
@@ -432,6 +184,7 @@ function createElement(element) {
     return rekke;
 };
 
+
 var searchCriteria = {
   kvinneSøk:false,
   herreSøk:false,
@@ -440,21 +193,83 @@ var searchCriteria = {
   gratisSøk:false,
   åpenNåSøk:false,
   maxPrisSøk:"",
-  åpenSøk:""
-
+  åpenSøk:"ikkeValgt",
+  adresseSøk:""
 }
-//Avansert søk!
-function avansertSøk(){
+
+function hurtigSøk(){
+  //lagrer søket i en tabell
+  var hSøk = document.getElementById("hurtigsøk").value;
+  var splittet = hSøk.split(" ");
+  for(var i = 0; i < splittet.length; i++){
+    if(splittet[i].includes(":")){
+      var nøkkel_verdi = splittet[i].split(":");
+      splittet[i] = nøkkel_verdi;
+    }
+  }
+  for(var i = 0; i < splittet.length; i++){
+    if(typeof(splittet[i])==="string"){
+      console.log(splittet[i]+" er en string");
+      if(splittet[i]=="herre"||splittet[i]=="mann"){
+        searchCriteria.herreSøk=true;
+      }else if(splittet[i]=="kvinne"||splittet[i]=="dame"){
+        searchCriteria.kvinneSøk=true;
+      }else if(splittet[i]=="rullestol"){
+        searchCriteria.rullestolSøk=true;
+      }else if(splittet[i]=="stellerom"){
+        searchCriteria.stelleromSøk=true;
+      }else if(splittet[i]=="gratis"){
+        searchCriteria.gratisSøk=true;
+      }else if(splittet[i]=="åpen"){
+        searchCriteria.åpenNåSøk=true;
+      }else if(splittet[i]!=""){
+        searchCriteria.adresseSøk=splittet[i];
+      }
+    }else{
+      if(splittet[i][0] == "kjønn"){
+        if(splittet[i][1] == "herre"||splittet[i][1]=="mann") searchCriteria.herreSøk = true;
+        if(splittet[i][1] == "dame"||splittet[i][1]=="kvinne") searchCriteria.kvinneSøk = true;
+      }
+      if(splittet[i][0] == "max"||splittet[i][0]=="maks") searchCriteria.maxPrisSøk = splittet[i][1];
+      if(splittet[i][0] == "åpent")searchCriteria.åpenSøk = splittet[i][1];
+
+    }
+    avansertSøk(searchCriteria);
+    if(hSøk==""){
+      tilbakestillSøk();
+    }
+
+  }
+}
+
+function skjemaSøk(){
+
   //true hvis kvinne er huket av, false hvis ikke.
-  var kvinneSøk = document.getElementById("kvinne").checked;
-  var herreSøk = document.getElementById("herre").checked;
-  var rullestolSøk = document.getElementById("rullestol").checked;
-  var stelleromSøk = document.getElementById("stellerom").checked;
-  var gratisSøk = document.getElementById("gratis").checked;
-  var åpenNåSøk = document.getElementById("åpenNå").checked;
-  var maxPrisSøk = document.getElementById("makspris").value;
-  var åpenSøk = document.getElementById("åpen").value;
-  var adresseSøk = document.getElementById("søkAdresse").value;
+  searchCriteria.kvinneSøk = document.getElementById("kvinne").checked;
+  searchCriteria.herreSøk = document.getElementById("herre").checked;
+  searchCriteria.rullestolSøk = document.getElementById("rullestol").checked;
+  searchCriteria.stelleromSøk = document.getElementById("stellerom").checked;
+  searchCriteria.gratisSøk = document.getElementById("gratis").checked;
+  searchCriteria.åpenNåSøk = document.getElementById("åpenNå").checked;
+  searchCriteria.maxPrisSøk = document.getElementById("makspris").value;
+  searchCriteria.åpenSøk = document.getElementById("åpen").value;
+  searchCriteria.adresseSøk = document.getElementById("søkAdresse").value;
+  avansertSøk(searchCriteria);
+}
+
+
+//Avansert søk!
+function avansertSøk(searchCriteria){
+ var kvinneSøk=searchCriteria.kvinneSøk;
+ var herreSøk=searchCriteria.herreSøk;
+ var rullestolSøk=searchCriteria.rullestolSøk;
+ var stelleromSøk = searchCriteria.stelleromSøk;
+ var gratisSøk = searchCriteria.gratisSøk;
+ var åpenNåSøk = searchCriteria.åpenNåSøk;
+ var maxPrisSøk = searchCriteria.maxPrisSøk;
+ var åpenSøk = searchCriteria.åpenSøk;
+ var adresseSøk = searchCriteria.adresseSøk;
+
 
   //Går igjennom hele listen
   for(var i = 0; i < toalettliste.length; i++){
@@ -582,7 +397,6 @@ function avansertSøk(){
         console.log("Fant adressen/navnet");
       }
       else{
-        console.log(adresse);
         document.getElementById(toalettliste[i].id).style.display = "none";
         markerArray[toalettliste[i].id-1].setMap(null);
       }
@@ -593,6 +407,15 @@ function avansertSøk(){
 function tilbakestillSøk(){
   console.log("fjernerfilter");
   for (var i = 0; i < toalettliste.length; i++) {
+    searchCriteria.kvinneSøk=false,
+    searchCriteria.herreSøk=false,
+    searchCriteria.rullestolSøk=false,
+    searchCriteria.stelleromSøk=false,
+    searchCriteria.gratisSøk=false,
+    searchCriteria.åpenNåSøk=false,
+    searchCriteria.maxPrisSøk="",
+    searchCriteria.åpenSøk="ikkeValgt",
+    searchCriteria.adresseSøk=""
     document.getElementById(toalettliste[i].id).style.display = "table-row";
     initMap();
     fjernAlleChecked();
@@ -607,5 +430,4 @@ function fjernAlleChecked(){
   document.getElementById("åpenNå").checked = false;
   document.getElementById("makspris").value = "";
   document.getElementById("søkAdresse").value = "";
-
 }
